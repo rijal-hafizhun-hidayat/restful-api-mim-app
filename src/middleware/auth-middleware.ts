277171735db44c18fa691e83818dce327974f038
unknown <rijal.1344@gmail.com>
2024-11-29 20:47:13 +0700

    feat: add auth middleware

---
+++ src/middleware/auth-middleware.ts
@@ -0,0 +1,75 @@
+import type { NextFunction, Response } from "express";
+import { TokenUtils } from "../utils/token-utils";
+import { prisma } from "../app/database";
+import type { CostumeRequest } from "../interface/request-interface";
+
+export const authMiddleware = async (
+  req: CostumeRequest,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res
+      .status(403)
+      .json({
+        statusCode: 403,
+        message: "no token provided",
+      })
+      .end();
+  }
+
+  const [, tokenValue] = token.split(" ");
+
+  try {
+    const decode = await TokenUtils.verifyToken(tokenValue);
+    const userId: number = (decode as any).userId;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      include: {
+        user_role: {
+          include: {
+            role: true,
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      return res
+        .status(403)
+        .json({
+          statusCode: 403,
+          errors: "token invalid",
+        })
+        .end();
+    }
+
+    const formatUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.user_role?.role,
+    };
+
+    req.currentUser = formatUser;
+  } catch (error: any) {
+    let errorMessage = "Token invalid";
+    if (error.name === "TokenExpiredError") {
+      errorMessage = "Token expired";
+    } else if (error.name === "JsonWebTokenError") {
+      errorMessage = "Token malformed";
+    }
+
+    return res
+      .status(403)
+      .json({
+        statusCode: 403,
+        errors: errorMessage,
+      })
+      .end();
+  }
+};
--- src/router/api-admin-router.ts
+++ src/router/api-admin-router.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { AuthController } from "../controller/admin/auth-controller";
+import { authMiddleware } from "../middleware/auth-middleware";
 
 const apiAdminRouter = express.Router();
 
 apiAdminRouter.post("/api/admin/login", AuthController.login);
+
+apiAdminRouter.use(authMiddleware);
 
 export { apiAdminRouter };
